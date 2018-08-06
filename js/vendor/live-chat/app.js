ffc088d1f8a474027b4435496dfef3c5d0cedf55
Matteo Cirami <matteocirami@Matteos-MacBook-Pro.local>
2018-08-06 16:41:55 -0500

    live stream page changes

--- js/vendor/live-chat/app.js
+++ js/vendor/live-chat/app.js
@@ -4,7 +4,6 @@ const pusher = new Pusher('5262ff28a038d2616775', {
 	authEndpoint: 'pusher/auth'
 });
 
-console.log(pusher);
 const app = new Vue({
 	el: '#app',
 	data: {
@@ -17,8 +16,12 @@ const app = new Vue({
 	},
 	methods: {
 		joinChat() {
-			axios.post('join-chat', {username: this.username})
+
+            var data = {username: this.username};
+
+			axios.post('wp-json/wp/v2/posts/', Qs.stringify(data))
 			.then(response => {
+                console.log(data);
 				// User has joined the chat
 				this.joined = true;
 				const channel = pusher.subscribe('presence-groupChat');
@@ -31,16 +34,59 @@ const app = new Vue({
 				});
 				// Listen for chat messages
 				this.listen();
-			});
+			})
+            .catch(function (error) {
+                console.log(error);
+            });
+
+            /*var protocol = window.location.protocol;
+            var data = JSON.stringify({username: this.username});
+            var domain = window.location.hostname;
+            var url = protocol + "//" + domain + "/wp-admin/admin-ajax.php";
+
+            $.ajax({
+                url: url,
+                type: "POST",
+                async: true,
+                dataType: 'json',
+                data: data,
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("X-WP-Nonce", currentUser.nonce);
+                    xhr.setRequestHeader("authorization", "OAuth oauth_consumer_key='IWmItGndx8oY',oauth_token='x3pmlsoef6ayQEdkasPgG01h',oauth_signature_method='HMAC-SHA1',oauth_timestamp='1497396491',oauth_nonce='Lqz1LK',oauth_version='1.0',oauth_signature='EnWnLRtpkruPc1bTtKVhMgECFWg%253D'");
+                    xhr.setRequestHeader("cache-control", "no-cache");
+                    xhr.setRequestHeader("postman-token", "25dc514c-3ad3-0c17-95e8-9dcc960c9ca0");
+                    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                },
+                success: function (data, xhr) {
+                    console.log(data);
+                    // User has joined the chat
+                    this.joined = true;
+                    const channel = pusher.subscribe('presence-groupChat');
+                    channel.bind('pusher:subscription_succeeded', (members) => {
+                        this.members = channel.members;
+                    });
+                    // User joins chat
+                    channel.bind('pusher:member_added', (member) => {
+                        this.status = `${member.id} joined the chat`;
+                    });
+                    // Listen for chat messages
+                    this.listen();
+                },
+                failure: function (data) {
+                    //alert(xhr.send(data));
+                    xhr.send(data);
+                    console.log(JSON.stringify(data));
+                }
+            })*/
 		},
 		sendMessage() {
 			let message = {
 				username: this.username,
 				message: this.newMessage
-			}
+			};
 			// Clear input field
 			this.newMessage = '';
-			axios.post('/send-message', message);
+			axios.post('wp-json/wp/v2/posts/', message);
 		},
 		listen() {
 			const channel = pusher.subscribe('presence-groupChat');
